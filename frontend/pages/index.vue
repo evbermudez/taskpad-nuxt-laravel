f4f16f527be316eb8c9a56a4786a8c1fad12162a
Eric Vincent Bermudez <eric.bermudez@gmail.com>
2025-10-09 03:49:19 +0800

    chore(frontend): clean Nuxt structure and confirm Tailwind + index page working

---
+++ frontend/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <main class="min-h-screen grid place-items-center p-10">
+    <h1 class="text-4xl font-extrabold">Tailwind is ON ✅</h1>
+  </main>
+</template>
+<script setup lang="ts"></script>
